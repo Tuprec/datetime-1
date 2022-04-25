13bc35ae1f83f69a161607670be4619aa74abe9f
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-04-25 08:42:52 +0200

    Add project files.

---
+++ datetime 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace datetime_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime tijd = new DateTime(2022, 4, 19);
+
+            Console.WriteLine(tijd.ToShortDateString());
+            Console.WriteLine(tijd.ToLongDateString());
+            Console.WriteLine(tijd.ToString());
+            Console.WriteLine($"{tijd.Day}/{tijd.Month}");
+            Console.WriteLine($"{tijd.ToShortDateString()} T {tijd.TimeOfDay} timezone");
+            Console.WriteLine(tijd.ToString("R"));
+            Console.WriteLine(tijd.ToShortTimeString());
+            Console.WriteLine(tijd.ToLongTimeString());
+            Console.WriteLine($"{tijd.Month}/{tijd.Year}");
+        }
+    }
+}
